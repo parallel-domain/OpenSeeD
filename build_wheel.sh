98362b55b65f2f22d8db6fbf35ae6375b61d50d5
Johan Vertens <johan.vertens@paralleldomain.com>
2026-03-07 15:40:54 +0100

    cleanup

--- build_wheel.sh
+++ build_wheel.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 export FORCE_CUDA=1
 export TORCH_CUDA_ARCH_LIST="8.6;8.9;9.0;12.0+PTX"
 
+rm -rf build dist *.egg-info dist
+
 pip wheel --no-deps --wheel-dir dist git+https://github.com/facebookresearch/detectron2.git@864913f0e57e87a75c8cc0c7d79ecbd774fc669b
 python setup.py bdist_wheel
 twine upload --repository-url https://pypi.internal.paralleldomain.com/ "dist/detectron2-0.6-cp311-cp311-linux_x86_64.whl"
